9e6193bf3cee20cad851b81e54fb902597c30cae
Florian Vögtle <f.voegtle@neusta.de>
2023-05-11 09:27:33 +0200

    First version

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,313 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+/// {@template my_app}
+/// This is the main application widget.
+/// {@endtemplate}
 class MyApp extends StatelessWidget {
+  /// {@macro my_app}
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo List',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorSchemeSeed: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+/// {@template todo_list_page}
+/// A page that shows a list of todos.
+/// {@endtemplate}
+class TodoListPage extends StatefulWidget {
+  /// {@macro todo_list_page}
+  const TodoListPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TodoListPage> createState() => _TodoListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoListPageState extends State<TodoListPage> {
+  static const _todosKey = 'todos';
+  late final SharedPreferences prefs;
+  late final List<TodoItem> todos;
+  bool isInitializing = true;
+  final TextEditingController _controllerTitle = TextEditingController();
+  final TextEditingController _controllerSubtitle = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((value) async {
+      prefs = value;
+      final savedTodos = value.getStringList(_todosKey);
+      await Future<void>.delayed(const Duration(seconds: 1));
+
+      setState(() {
+        todos = savedTodos
+                ?.map(
+                  (e) => TodoItem.fromJson(
+                    jsonDecode(e) as Map<String, dynamic>,
+                  ),
+                )
+                .toList() ??
+            [];
+        isInitializing = false;
+      });
+      _controllerSubtitle.addListener(() {
+        setState(() {});
+      });
+      _controllerTitle.addListener(() {
+        setState(() {});
+      });
     });
   }
 
   @override
+  void dispose() {
+    _controllerSubtitle.dispose();
+    _controllerTitle.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Todo List'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Builder(
+        builder: (context) {
+          if (isInitializing) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Stack(
+            children: [
+              ListView.builder(
+                padding: const EdgeInsets.all(16),
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  final todo = todos[index];
+                  return TodoListItem(
+                    key: ValueKey(todo.id),
+                    title: Text(todo.title),
+                    subtitle: Text(todo.subtitle),
+                    onChecked: () => setState(() {
+                      removeTodoAtIndex(index);
+                    }),
+                  );
+                },
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Card(
+                  elevation: 6,
+                  margin: const EdgeInsets.all(36),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                controller: _controllerTitle,
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.all(16),
+                                  hintText: 'Title',
+                                ),
+                                onFieldSubmitted: (value) {
+                                  addTodo();
+                                },
+                              ),
+                              TextFormField(
+                                controller: _controllerSubtitle,
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.all(16),
+                                  hintText: 'Subtitle',
+                                ),
+                                onFieldSubmitted: (value) {
+                                  addTodo();
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 100,
+                          width: 1,
+                          color: Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(0.1),
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.add,
+                            color: _controllerSubtitle.text.isEmpty ||
+                                    _controllerTitle.text.isEmpty
+                                ? Theme.of(context)
+                                    .colorScheme
+                                    .primary
+                                    .withOpacity(0.4)
+                                : Theme.of(context).colorScheme.primary,
+                          ),
+                          onPressed: _controllerSubtitle.text.isEmpty ||
+                                  _controllerTitle.text.isEmpty
+                              ? null
+                              : addTodo,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  void addTodo() {
+    setState(() {
+      todos.add(
+        TodoItem(
+          title: _controllerTitle.text,
+          subtitle: _controllerSubtitle.text,
+        ),
+      );
+      prefs.setStringList(_todosKey, todos.map(jsonEncode).toList());
+      _controllerTitle.clear();
+      _controllerSubtitle.clear();
+    });
+  }
+
+  void removeTodoAtIndex(int index) {
+    todos.removeAt(index);
+
+    prefs.setStringList(_todosKey, todos.map(jsonEncode).toList());
+  }
+}
+
+/// {@template todo_list_item}
+/// A list item that shows a todo_item.
+/// {@endtemplate}
+class TodoListItem extends StatefulWidget {
+  /// {@macro todo_list_item}
+  const TodoListItem({
+    required this.title,
+    required this.subtitle,
+    super.key,
+    this.onChecked,
+  });
+
+  /// The title of the todo_item.
+  final Widget title;
+
+  /// The subtitle of the todo_item.
+  final Widget subtitle;
+
+  /// Called when the todo_item is checked.
+  final VoidCallback? onChecked;
+
+  @override
+  State<TodoListItem> createState() => _TodoListItemState();
+}
+
+class _TodoListItemState extends State<TodoListItem> {
+  late var _checked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        title: widget.title,
+        subtitle: widget.subtitle,
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Checkbox(
+              value: _checked,
+              onChanged: (_) {
+                widget.onChecked?.call();
+                setState(() {
+                  _checked = true;
+                });
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+/// {@template todo_item}
+/// A todo_item.
+/// {@endtemplate}
+class TodoItem {
+  /// {@macro todo_item}
+  factory TodoItem({
+    required String title,
+    required String subtitle,
+  }) {
+    return TodoItem._(
+      title: title,
+      subtitle: subtitle,
+      id: const Uuid().v4(),
+    );
+  }
+
+  /// Returns a todo_item from a json map.
+  factory TodoItem.fromJson(Map<String, dynamic> json) {
+    return TodoItem._(
+      title: json['title'] as String,
+      subtitle: json['subtitle'] as String,
+      id: json['id'] as String,
+    );
+  }
+
+  TodoItem._({
+    required this.title,
+    required this.subtitle,
+    required this.id,
+  });
+
+  /// The id of the todo_item.
+  late final String id;
+
+  /// The title of the todo_item.
+  final String title;
+
+  /// The subtitle of the todo_item.
+  final String subtitle;
+
+  /// Returns a json map from the todo_item.
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'subtitle': subtitle,
+    };
   }
 }
